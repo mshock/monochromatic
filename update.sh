75be994b4e289f7d6c2166456ef37e128aae7221
Willy Goiffon <w.goiffon@gmail.com>
2013-08-08 19:06:30 +0200

    now includes the blank div

--- update.sh
+++ update.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 for file in $(find . -name '*.html'); do
-    if test $(grep -E '</header>&<footer>' $file); then
-        sed '1,/<\/header>/d;/<footer>/,$d' $file
-        cat head.template $file foot.template > buffer.html
+    if [[ -n "$(cat $file|tr -d '\n' |grep -E '</header>.*<footer')" ]]; then
+        cat head.template > buffer.html
+        sed '1,/<\/header>/d;/<div id=.blank.>/,$d' $file >> buffer.html
+        cat foot.template >> buffer.html
+
+        mv buffer.html $file
     fi
 done
