2a039c60bf8aa5b7774895e7a95f6056fd5a6744
Willy Goiffon <w.goiffon@gmail.com>
2013-08-09 00:12:16 +0200

    changed footer to be less visible (and removed div spacer)

--- update.sh
+++ update.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 for file in $(find . -name '*.html'); do
-    if [[ -n "$(cat $file|tr -d '\n' |grep -E '</header>.*<footer')" ]]; then
+    if [[ -n "$(cat $file|tr -d '\n' |grep -E '</header>.*<footer>')" ]]; then
         cat head.template > buffer.html
-        sed '1,/<\/header>/d;/<div id=.blank.>/,$d' $file >> buffer.html
+        sed '1,/<\/header>/d;/<footer>/,$d' $file >> buffer.html
         cat foot.template >> buffer.html
 
         mv buffer.html $file
