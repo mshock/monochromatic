bf6298c85a4c337d2df3c4499d16f28274c6a293
Willy Goiffon <w.goiffon@gmail.com>
2013-08-09 15:51:49 +0200

    fixed the footer update

--- update.sh
+++ update.sh
@@ -3,7 +3,7 @@
 for file in $(find . -name '*.html'); do
     if [[ -n "$(cat $file|tr -d '\n' |grep -E '</header>.*<footer>')" ]]; then
         cat head.template > buffer.html
-        sed '1,/<\/header>/d;/<footer>/,$d' $file >> buffer.html
+        sed '1,/<\/header>/d;/<!-- footer/,$d' $file >> buffer.html
         cat foot.template >> buffer.html
 
         mv buffer.html $file
