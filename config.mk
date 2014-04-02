de4e65b4240ca5739b3e1d7e210d98a29637fea3
z3bra <willy@mailoo.org>
2014-04-02 12:33:49 +0200

    New article about status bar

--- config.mk
+++ config.mk
@@ -4,16 +4,16 @@ NAME    = monochromatic
 PREFIX  = /srv/http/blog.z3bra.org
 
 PAGES   =   index.html \
-	    2013/12/love-me-some-latex.html \
-	    2013/11/plain-old-mails.html \
-	    2013/10/home-sweet-home.html \
+	    about.html \
 	    2013/08/test-your-css.html \
 	    2013/08/the-hard-way.html \
 	    2013/09/java-without-eclipse.html \
-	    2014/03/unleash-your-desktop.html \
-	    2014/03/toolbox.html \
+	    2013/10/home-sweet-home.html \
+	    2013/11/plain-old-mails.html \
+	    2013/12/love-me-some-latex.html \
 	    2014/01/images-in-terminal.html \
-	    about.html
+	    2014/03/toolbox.html \
+	    2014/04/meeting-at-the-bar.html
 
 FEEDS = rss/feed.xml
 EXTRA = css img favicon.ico
